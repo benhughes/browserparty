5c1beed24148b2bdbe3589d6ce834aad43fd0def
Benjamin Hughes <benhuggy@gmail.com>
2012-11-06 00:41:31 +0000

    Ability to run tests with Jasmine
    Followed intstructions from
    http://kilon.org/blog/2012/08/testing-backbone-requirejs-applications-wi
    th-jasmine/ to run tests using requirejs and jasmine

---
+++ public/unit-tests/specRunner.js
@@ -0,0 +1,56 @@
+require.config({
+  baseUrl: '/js/',
+  paths: {
+    'jquery': 'lib/jquery/jquery-1.8.2.min',
+    'underscore': 'lib/underscore/underscore',
+    'backbone': 'lib/backbone/backbone',
+    'socket': '../socket.io/socket.io',
+    'text': 'lib/require/plugins/text',
+    'log': 'log/log',
+    //Jasmine
+    'jasmine': '../unit-tests/jasmine-1.2.0/jasmine',
+    'jasmine-html': '../unit-tests/jasmine-1.2.0/jasmine-html'
+  },
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    },
+    jasmine: {
+      exports: 'jasmine'
+    },
+    'jasmine-html': {
+      deps: ['jasmine'],
+      exports: 'jasmine'
+    }
+  }
+});
+
+require(['underscore', 'jquery', 'jasmine-html'], function(_, $, jasmine){
+ 
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+ 
+  var htmlReporter = new jasmine.HtmlReporter();
+ 
+  jasmineEnv.addReporter(htmlReporter);
+ 
+  jasmineEnv.specFilter = function(spec) {
+    return htmlReporter.specFilter(spec);
+  };
+ 
+  var specs = [];
+ 
+  specs.push('../unit-tests/tests/pubsub');
+ 
+ 
+  $(function(){
+    require(specs, function(){
+      jasmineEnv.execute();
+    });
+  });
+ 
+});
