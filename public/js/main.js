d67dafb5fa9e8577c4c190a0e11601f429cc9539
Ben Hughes <benhuggy@gmail.com>
2012-11-19 23:01:34 +0000

    Swapped spaces for tabs and added export for socket

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,33 @@
-(function(){
+(function () {
 
 	require.config({
 		paths: {
-	    	'jquery': 'lib/jquery/jquery-1.8.2.min',
-	    	'underscore': 'lib/underscore/underscore',
-	    	'backbone': 'lib/backbone/backbone',
-	    	'socket': '../socket.io/socket.io',
-	    	'text': 'lib/require/plugins/text',
-	    	'log': 'log/log'
-	  	},
-	 	shim: {
-	 		underscore: {
-      			exports: '_'
-      		},
-      		backbone: {
-      			deps: ["underscore", "jquery"],
-      			exports: "Backbone"
-      		}
-      	}
+			'jquery': 'lib/jquery/jquery-1.8.2.min',
+			'underscore': 'lib/underscore/underscore',
+			'backbone': 'lib/backbone/backbone',
+			'socket': '../socket.io/socket.io',
+			'text': 'lib/require/plugins/text',
+			'log': 'log/log'
+		},
+		shim: {
+			underscore: {
+				exports: '_'
+			},
+			backbone: {
+				deps: ["underscore", "jquery"],
+				exports: "Backbone"
+			},
+			socket: {
+				exports: "io"
+			}
+		}
 	});
 
 	require([
-			'app'
-		], 
-		function(app) {
-			app.init();
-		}
-	);
+		'app'
+	],
+	function (app) {
+		app.init();
+	});
 
-})()
+})();
