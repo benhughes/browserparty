3985c2f446d5bcfba55a7965b1d8e8e4076e4592
Ben Hughes <benhuggy@gmail.com>
2012-11-19 21:35:12 +0000

    Improved Log
    Added a seperator between the first arg and the rest as the first one
    should be the indentifier of the module that calls it

--- public/js/log/log.js
+++ public/js/log/log.js
@@ -1,11 +1,15 @@
 define(function () {
 	var history = [];
 	return function () {
+		var args = Array.prototype.slice.call(arguments);
+		args.splice(1, 0, "--");
+		args[0] = "[" + args[0] + "]";
 		history = history || [];   // store logs to an array for reference
-		history.push(arguments);
+		history.push(args);
+
 		if (console) {
-		  var log = Function.prototype.bind.call(console.log, console);
-			log.apply(console, arguments);
+			var log = Function.prototype.bind.call(console.log, console);
+			log.apply(console, args);
 		}
 	};
 });
