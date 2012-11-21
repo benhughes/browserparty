7c6a0932f21b644f9119d00f1ac6c436be18f21c
Ben Hughes <benhuggy@gmail.com>
2012-11-21 20:21:55 +0000

    JsLintd the file

--- app.js
+++ app.js
@@ -3,19 +3,18 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , _ = require('underscore')._
-  , backbone = require('backbone');
-
-var app = express(),
-  server = http.createServer(app)
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path'),
+  _ = require('underscore')._,
+  backbone = require('backbone'),
+  app = express(),
+  server = http.createServer(app),
   io = require('socket.io').listen(server);
 
-app.configure(function(){
+app.configure(function () {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -30,38 +29,37 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-server.listen(app.get('port'), function(){
+server.listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
 
 app.listen();
- 
 io.sockets.on('connection', function (socket) {
-    console.log('A socket connected!', socket.id);
-    socket.on('message', function (msg) {
-        console.log('Message Received: ', msg);
-        socket.broadcast.emit('message', msg);
-    });
-    socket.emit('clientUpdate', {
-      id: socket.id
-    });
-    socket.on('clientUpdate', function (data) {
-      console.log('Client Update recieved', data)
-      socket.emit('clientUpdate', data);
-      socket.broadcast.emit('connectonsUpdate', _.extend(data, {id: socket.id}));
-    });
-    socket.on('test', function (msg) {
-        console.log('Message Received: ', msg);
-        socket.broadcast.emit('message', msg.message);
-    });
-    socket.on('browserInfo', function(info){
-      socket.broadcast.emit('message', info.userAgent);
-    })
+  console.log('A socket connected!', socket.id);
+  socket.on('message', function (msg) {
+    console.log('Message Received: ', msg);
+    socket.broadcast.emit('message', msg);
+  });
+  socket.emit('clientUpdate', {
+    id: socket.id
+  });
+  socket.on('clientUpdate', function (data) {
+    console.log('Client Update recieved', data);
+    socket.emit('clientUpdate', data);
+    socket.broadcast.emit('connectonsUpdate', _.extend(data, {id: socket.id}));
+  });
+  socket.on('test', function (msg) {
+    console.log('Message Received: ', msg);
+    socket.broadcast.emit('message', msg.message);
+  });
+  socket.on('browserInfo', function (info) {
+    socket.broadcast.emit('message', info.userAgent);
+  });
 });
