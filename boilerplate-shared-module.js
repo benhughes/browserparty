a64cfbfaa308b1260b6f3df595fa81854821531f
Ben Hughes <benhuggy@gmail.com>
2012-12-01 18:46:36 +0000

    Added a boilerplate for shared modules

---
+++ boilerplate-shared-module.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    var moduleName = function (_, Backbone, log) {
+        var logPrefix = "----boilerplate change me----";
+
+        return {};
+    };
+    //sets up for require to be able to use file in browser and commonjs to use it on serevr
+    if (typeof define === "function" && define.amd) {
+        define(['underscore', 'backbone', 'log'], moduleName);
+    } else if (module && typeof module.exports !== 'undefined') {
+        module.exports = moduleName(require('underscore')._, require('backbone'), require('../log/log'));
+    }
+
+}());
