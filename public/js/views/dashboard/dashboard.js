007cc26bd2ef65893dfa8087f09f8faecd57e727
Ben Hughes <benhuggy@gmail.com>
2012-12-29 09:24:17 +0000

    Added some basic styling

--- public/js/views/dashboard/dashboard.js
+++ public/js/views/dashboard/dashboard.js
@@ -1,48 +1,49 @@
 // Filename: views/project/list
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'log',
-  'text!/templates/dashboard/dashboard.html',
-  'collections/connections/connections'
+	'jquery',
+	'underscore',
+	'backbone',
+	'log',
+	'text!/templates/dashboard/dashboard.html',
+	'collections/connections/connections'
 ], function ($, _, Backbone, log, dashboardTemplate) {
-  var logPrefix = "views/dashboard/dashboard",
-    Dashboard = Backbone.View.extend({
-      el: $('#container'),
-      modules: [
-          {
-              id: "connections",
-              viewLoc: "views/connectionModule/connectionModule"
-          },
-          {
-              id: "events",
-              viewLoc: "views/eventsModule/eventsModule"
-          }
-      ],
-      render: function () {
-        var data = {},
-          compiledTemplate = _.template(dashboardTemplate, data);
+	var logPrefix = "views/dashboard/dashboard",
+		Dashboard = Backbone.View.extend({
+			className: 'dashboard',
+			modules: [
+				{
+					id: "connections",
+					viewLoc: "views/connectionModule/connectionModule"
+				},
+				{
+					id: "events",
+					viewLoc: "views/eventsModule/eventsModule"
+				}
+			],
+			render: function () {
+				var data = {},
+					compiledTemplate = _.template(dashboardTemplate, data);
 
-        // Append our compiled template to this Views "el"
-        this.$el.append(compiledTemplate);
-        this.renderModules();
-      },
-      renderModules: function () {
-        _.each(this.modules, function (module) {
-          log(logPrefix, 'Rendering module', module.id);
+				this.el.innerHTML = compiledTemplate;
+				$('#container').html(this.el);
+				this.renderModules();
+			},
+			renderModules: function () {
+				_.each(this.modules, function (module) {
+					log(logPrefix, 'Rendering module', module.id);
 
-          _.bindAll(this, 'moduleRender');
+					_.bindAll(this, 'moduleRender');
 
-          require([module.viewLoc], this.moduleRender);
-        }, this);
-      },
-      moduleRender: function (ModuleView) {
-        moduleView = new ModuleView();
-        this.$el.append(moduleView.render());
+					require([module.viewLoc], this.moduleRender);
+				}, this);
+			},
+			moduleRender: function (ModuleView) {
+				moduleView = new ModuleView();
+				log('---', this.el)
+				$(this.el).append(moduleView.render());
 
-      }
-    });
-  // Our module now returns our view
-  return Dashboard;
+			}
+		});
+	// Our module now returns our view
+	return Dashboard;
 });
